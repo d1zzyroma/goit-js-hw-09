c37833cd8c05dd50e03b88325872a95086022e87
d1zzyroma <nataliawologin@gmail.com>
2024-05-11 13:10:58 +0200

    123

---
+++ src/js/2-form.js
@@ -0,0 +1,70 @@
+const formData = {
+    email:"",
+    message: ""
+}
+
+
+const form = document.querySelector(".feedback-form")
+
+const getFormData = localStorage.getItem("feedback-form-state");
+
+const parseFormData = JSON.parse(getFormData);
+
+if(parseFormData){
+    form.elements.email.value = parseFormData.email
+    form.elements.message.value = parseFormData.message
+
+    formData.email = parseFormData.email;
+    formData.message = parseFormData.message;
+}else{
+    form.elements.email.value = "";
+    form.elements.message.value = "";
+}
+
+
+
+form.addEventListener("input", handelInput);
+function handelInput(event){
+
+    if(event.target.name == "email"){
+        formData.email = event.target.value;
+    }else if(event.target.name == "message"){
+        formData.message = event.target.value
+    }
+    
+}
+
+if(!form.elements.email.value || !form.elements.email.value){
+    alert("Fill please all fields")
+}
+
+form.addEventListener("submit", handelSubmit);
+
+function handelSubmit(event){
+    
+
+    if(!form.elements.email.value || !form.elements.message.value){
+        event.preventDefault()
+        alert("Fill please all fields")
+    }else{
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
