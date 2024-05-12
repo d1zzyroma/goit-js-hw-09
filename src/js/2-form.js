0a4e0a1106f55036542f6ce353c80afe1eb90cb4
d1zzyroma <nataliawologin@gmail.com>
2024-05-12 15:00:08 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,8 +11,8 @@ const getFormData = localStorage.getItem("feedback-form-state");
 const parseFormData = JSON.parse(getFormData);
 
 if(parseFormData){
-    form.elements.email.value = parseFormData.email
-    form.elements.message.value = parseFormData.message
+    form.elements.email.value = parseFormData.email;
+    form.elements.message.value = parseFormData.message;
 
     formData.email = parseFormData.email;
     formData.message = parseFormData.message;
@@ -29,20 +29,15 @@ function handelInput(event){
     if(event.target.name == "email"){
         formData.email = event.target.value;
     }else if(event.target.name == "message"){
-        formData.message = event.target.value
+        formData.message = event.target.value;
     }
     
 }
 
-if(!form.elements.email.value || !form.elements.email.value){
-    alert("Fill please all fields")
-}
 
 form.addEventListener("submit", handelSubmit);
 
 function handelSubmit(event){
-    
-
     if(!form.elements.email.value || !form.elements.message.value){
         event.preventDefault()
         alert("Fill please all fields")
@@ -50,6 +45,8 @@ function handelSubmit(event){
         localStorage.setItem("feedback-form-state", JSON.stringify(formData))
     }
 }
+
+
 
 
 
