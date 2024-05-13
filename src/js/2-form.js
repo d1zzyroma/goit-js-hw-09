c878da589de046faa34fa189aaa909121f287b77
d1zzyroma <nataliawologin@gmail.com>
2024-05-13 17:02:18 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,67 +1,50 @@
 const formData = {
-    email:"",
+    email: "",
     message: ""
-}
+};
 
+const form = document.querySelector(".feedback-form");
 
-const form = document.querySelector(".feedback-form")
-
+// Перевіряємо, чи є дані в localStorage
 const getFormData = localStorage.getItem("feedback-form-state");
-
 const parseFormData = JSON.parse(getFormData);
 
-if(parseFormData){
+// Встановлюємо значення полів форми, якщо є дані в localStorage
+if (parseFormData) {
     form.elements.email.value = parseFormData.email;
     form.elements.message.value = parseFormData.message;
 
     formData.email = parseFormData.email;
     formData.message = parseFormData.message;
-}else{
+} else {
     form.elements.email.value = "";
     form.elements.message.value = "";
 }
 
-
-
-form.addEventListener("input", handelInput);
-function handelInput(event){
-
-    if(event.target.name == "email"){
+// Слухач подій для введення даних в форму
+form.addEventListener("input", handleInput);
+function handleInput(event) {
+    if (event.target.name === "email") {
         formData.email = event.target.value;
-    }else if(event.target.name == "message"){
+    } else if (event.target.name === "message") {
         formData.message = event.target.value;
     }
-    
 }
 
 
-form.addEventListener("submit", handelSubmit);
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault()
+    if (!form.elements.email.value || !form.elements.message.value) {
+        event.preventDefault();
+        alert("Please fill in all fields.");
+    } else {
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+        
+        console.log("Form Data:", formData);
 
-function handelSubmit(event){
-    if(!form.elements.email.value || !form.elements.message.value){
-        event.preventDefault()
-        alert("Fill please all fields")
-    }else{
-        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+        
+        formData.email = "";
+        formData.message = "";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
